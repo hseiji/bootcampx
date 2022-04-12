6cf70844259a05ac947355db4b98c6289da00394
hseiji <henrique.hst@gmail.com>
2022-04-11 20:34:25 -0400

    add node postgres to teachers and students

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -12,16 +12,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name as name, cohorts.name as cohort_id
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
-.then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
-    })
-})
-.catch(err => console.error('query error', err.stack));
+const text =  "SELECT students.id, students.name as name, cohorts.name as cohort_id FROM students JOIN cohorts ON cohorts.id = students.cohort_id WHERE cohorts.name LIKE $1 LIMIT $2;";
+const values = ['%' + cohort + '%', limit];
+
+pool
+  .query(text, values)
+  .then(res => {
+      res.rows.forEach(user => {
+        console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+      })
+  })
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%';`)
-.then(res => {
-    res.rows.forEach(data => {
-      console.log(`${data.cohort}: ${data.teacher}`);
-    })
-})
-.catch(err => console.error('query error', err.stack));
+const text = "SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort FROM teachers JOIN assistance_requests ON teacher_id = teachers.id JOIN students ON students.id = student_id JOIN cohorts ON cohorts.id = cohort_id WHERE cohorts.name LIKE $1;";
+const values = ['%' + process.argv[2] + '%'];
+
+pool
+  .query(text, values)
+  .then(res => {
+      res.rows.forEach(data => {
+        console.log(`${data.cohort}: ${data.teacher}`);
+      })
+  })
+  .catch(err => console.error('query error', err.stack));
